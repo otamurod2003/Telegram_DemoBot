814d754c4f12a16afa096d454ff073fe320dbffd
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-02-13 11:45:33 +0500

    created new telegrambot empty

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using Renci.SshNet;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+class Program
+{
+    private static TelegramBotClient client;
+    static void Main(string[] args)
+    {
+        // token, который вернул BotFather
+        client = new TelegramBotClient(token);
+        client.OnMessage += BotOnMessageReceived;
+        client.OnMessageEdited += BotOnMessageReceived;
+        client.StartReceiving();
+        Console.ReadLine();
+        client.StopReceiving();
+    }
+    private async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+    {
+        var message = messageEventArgs.Message;
+        if (message?.Type == MessageType.TextMessage)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, message.Text);
+        }
+    }
+}
---
+++ ProgramBase.cs
@@ -0,0 +1,25 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+internal class ProgramBase
+{
+    private static TelegramBotClient client;
+    static void Main(string[] args)
+    {
+        // token, который вернул BotFather
+        client = new TelegramBotClient(token);
+        client.OnMessage += BotOnMessageReceived;
+        client.OnMessageEdited += BotOnMessageReceived;
+        client.StartReceiving();
+        Console.ReadLine();
+        client.StopReceiving();
+    }
+    private async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+    {
+        var message = messageEventArgs.Message;
+        if (message?.Type == MessageType.TextMessage)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, message.Text);
+        }
+    }
+}
